3499f050de5bcd88a0db71518ebadebda6795066
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2026-06-17 03:10:57 +0200

    Obfuscation: Setup passes in the optimization pipeline
    TODO: Check if the passes actually survive optimization.

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1586,6 +1586,14 @@ PassBuilder::buildPerModuleDefaultPipeline(OptimizationLevel Level,
   // Force any function attributes we want the rest of the pipeline to observe.
   MPM.addPass(ForceFunctionAttrsPass());
 
+  MPM.addPass(StringEncryptionPass());
+
+  FunctionPassManager FPM;
+  FPM.addPass(BogusControlFlowPass());
+  FPM.addPass(SplitBasicBlockPass());
+  FPM.addPass(FlatteningPass());
+  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
+
   if (PGOOpt && PGOOpt->DebugInfoForProfiling)
     MPM.addPass(createModuleToFunctionPassAdaptor(AddDiscriminatorsPass()));
 
@@ -1610,6 +1618,17 @@ PassBuilder::buildPerModuleDefaultPipeline(OptimizationLevel Level,
 
   if (LTOPreLink)
     addRequiredLTOPreLinkPasses(MPM);
+
+  MPM.addPass(IPObfuscationContextPass());
+
+  FPM = FunctionPassManager();
+  FPM.addPass(IndirectCallPass());
+  FPM.addPass(SubstitutionPass());
+  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
+
+  MPM.addPass(IndirectBranchPass());
+  MPM.addPass(IndirectGlobalVariablePass());
+
   return MPM;
 }
 
