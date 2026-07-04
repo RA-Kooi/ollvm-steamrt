b14a1425f3118db603d51853ddd6671d763a86c1
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2026-07-04 17:46:48 +0200

    Obfuscation: Adjust pass order
    Make the control flow related pass run last.

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1759,16 +1759,7 @@ PassBuilder::buildPerModuleDefaultPipeline(OptimizationLevel Level,
     addRequiredLTOPreLinkPasses(MPM);
 
   FunctionPassManager FPM;
-  FPM.addPass(AliasAccess());
-  FPM.addPass(BogusControlFlowPass());
-  FPM.addPass(SplitBasicBlockPass());
-  FPM.addPass(FlatteningPass());
-  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
-
-  FPM = FunctionPassManager();
-  FPM.addPass(LinearMBA());
   FPM.addPass(IndirectBranchPass());
-  FPM.addPass(SubstitutionPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
   MPM.addPass(StringEncryptionPass());
@@ -1776,6 +1767,14 @@ PassBuilder::buildPerModuleDefaultPipeline(OptimizationLevel Level,
 
   FPM = FunctionPassManager();
   FPM.addPass(IndirectGlobalVariablePass());
+  FPM.addPass(LinearMBA());
+  FPM.addPass(SubstitutionPass());
+  FPM.addPass(AliasAccess());
+  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
+
+  FPM.addPass(BogusControlFlowPass());
+  FPM.addPass(SplitBasicBlockPass());
+  FPM.addPass(FlatteningPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
   return MPM;
@@ -2457,13 +2456,7 @@ PassBuilder::buildO0DefaultPipeline(OptimizationLevel Level,
   invokeOptimizerLastEPCallbacks(MPM, Level, Phase);
 
   FunctionPassManager FPM;
-  FPM.addPass(AliasAccess());
-  FPM.addPass(BogusControlFlowPass());
-  FPM.addPass(SplitBasicBlockPass());
-  FPM.addPass(FlatteningPass());
-  FPM.addPass(LinearMBA());
   FPM.addPass(IndirectBranchPass());
-  FPM.addPass(SubstitutionPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
   MPM.addPass(StringEncryptionPass());
@@ -2471,6 +2464,14 @@ PassBuilder::buildO0DefaultPipeline(OptimizationLevel Level,
 
   FPM = FunctionPassManager();
   FPM.addPass(IndirectGlobalVariablePass());
+  FPM.addPass(LinearMBA());
+  FPM.addPass(SubstitutionPass());
+  FPM.addPass(AliasAccess());
+  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
+
+  FPM.addPass(BogusControlFlowPass());
+  FPM.addPass(SplitBasicBlockPass());
+  FPM.addPass(FlatteningPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
   if (isLTOPreLink(Phase))
