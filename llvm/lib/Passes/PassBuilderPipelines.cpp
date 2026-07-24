d3944f208403b2c02c3c651c8e01d0b5c8862386
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2026-07-24 03:32:32 +0200

    Passes: Run indirect branch pass after flattening

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1613,23 +1613,21 @@ PassBuilder::buildPerModuleDefaultPipeline(OptimizationLevel Level,
   if (LTOPreLink)
     addRequiredLTOPreLinkPasses(MPM);
 
-  FunctionPassManager FPM;
-  FPM.addPass(IndirectBranchPass());
-  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
-
   MPM.addPass(StringEncryptionPass());
   MPM.addPass(IndirectCallPass());
 
-  FPM = FunctionPassManager();
+  FunctionPassManager FPM;
   FPM.addPass(IndirectGlobalVariablePass());
   FPM.addPass(LinearMBA());
   FPM.addPass(SubstitutionPass());
   FPM.addPass(AliasAccess());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
+  FPM = FunctionPassManager();
   FPM.addPass(BogusControlFlowPass());
   FPM.addPass(SplitBasicBlockPass());
   FPM.addPass(FlatteningPass());
+  FPM.addPass(IndirectBranchPass());
   FPM.addPass(AntiAnalysisJumpPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
@@ -2189,23 +2187,21 @@ ModulePassManager PassBuilder::buildO0DefaultPipeline(OptimizationLevel Level,
 
   invokeOptimizerLastEPCallbacks(MPM, Level);
 
-  FunctionPassManager FPM;
-  FPM.addPass(IndirectBranchPass());
-  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
-
   MPM.addPass(StringEncryptionPass());
   MPM.addPass(IndirectCallPass());
 
-  FPM = FunctionPassManager();
+  FunctionPassManager FPM;
   FPM.addPass(IndirectGlobalVariablePass());
   FPM.addPass(LinearMBA());
   FPM.addPass(SubstitutionPass());
   FPM.addPass(AliasAccess());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
+  FPM = FunctionPassManager();
   FPM.addPass(BogusControlFlowPass());
   FPM.addPass(SplitBasicBlockPass());
   FPM.addPass(FlatteningPass());
+  FPM.addPass(IndirectBranchPass());
   FPM.addPass(AntiAnalysisJumpPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 
